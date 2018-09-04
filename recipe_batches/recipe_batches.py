f96c426b743871c9888dad7bf083f4f8796e27ea
nickolaussmith <nickolaussmith721@gmail.com>
2018-09-04 13:54:56 -0600

    Recipes baked

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,19 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass
+  batch = []                        # Empty array for our ingredients
+  for ingredient in  recipe.keys(): # Grabbing names of the required ingredients in the recipe
+    if ingredient not in ingredients: # If ingredients doesn't have an ingredient from recipe, no batches can be made
+      return 0
+    else:
+       batch.append(ingredients[ingredient] // recipe[ingredient]) # Adds the number of batches per ingredient(s)
+       
+  return min(batch) # Returns how many batches we can make
+      
+
+
+
+ 
 
 
 if __name__ == '__main__':
@@ -12,3 +24,8 @@ if __name__ == '__main__':
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+
+   # if ingredients has x, y, z from recipe:
+  #   check quantity of x, y, z
+  # else:
+  #   not enough x, y, z/ no x, y, z at all
